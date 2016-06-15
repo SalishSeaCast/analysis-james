913560657d5bfb6e8670871a05190343f1c52392
James Petrie <jpetrie@eos.ubc.ca>
2016-06-15 13:33:17 -0700

    very minor changes

--- generate_analysis_notebooks.py
+++ generate_analysis_notebooks.py
@@ -3,7 +3,7 @@ from nbconvert.preprocessors import ExecutePreprocessor
 from os import listdir
 import os
 import sys
-
+from threading import Thread
 
 def create_notebook(reference_ipynb, dest_file_path, result_dir, var_name):
     result_dir_placeholder = "INSERT_RESULTS_DIRECTORY"
@@ -18,34 +18,38 @@ def run_notebook(notebook_filename):
     with open(notebook_filename) as f:
         nb = nbformat.read(f, as_version=4)
     ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
-    ep.preprocess(nb, {'metadata': {'path': '/ocean/jpetrie/MEOPAR/analysis-james/notebooks/'}})
-    with open(notebook_filename, 'wt') as f:
-        nbformat.write(nb, f)
+    try:
+        ep.preprocess(nb, {'metadata': {'path': '/ocean/jpetrie/MEOPAR/analysis-james/notebooks/'}})
+        with open(notebook_filename, 'wt') as f:
+            nbformat.write(nb, f)
+    except:
+        print(notebook_filename + " failed to run")
 
 
-def create_and_run_notebooks(analysis_dir, results_dir, reference_ipynb):
+def create_and_run_notebooks(analysis_dir, result_dir, reference_ipynb):
     if not os.path.exists(analysis_dir):
         os.makedirs(analysis_dir)
 
     var_names = set()
-    for file_name in listdir(results_dir):
-        last_underscore = file_name.rfind("_")
-        var_name = file_name[:last_underscore]
-        var_names.add(var_name)
+    for file_name in listdir(result_dir):
+        if not file_name.startswith('nampiszoo_zz_frac_waste'):
+            last_underscore = file_name.rfind("_")
+            var_name = file_name[:last_underscore]
+            var_names.add(var_name)
 
     for var_name in var_names:
         print(var_name)
-        notebook_file = analysis_dir + var_name + '.ipynb'
-        create_notebook(reference_ipynb, notebook_file, results_dir, var_name)
+        notebook_file = analysis_dir + '/' + var_name + '.ipynb'
+        create_notebook(reference_ipynb, notebook_file, result_dir, var_name)
         print("running " + var_name)
         run_notebook(notebook_file)
 
 if __name__ == '__main__':
     analysis_dir = sys.argv[1]
-    results_dir = sys.argv[2]
+    result_dir = sys.argv[2]
     if len(sys.argv) > 3:
         reference_ipynb = sys.argv[3]
     else:
         reference_ipynb = '/ocean/jpetrie/MEOPAR/analysis-james/notebooks/parameter_analysis_template.ipynb'
     print(analysis_dir)
-    create_and_run_notebooks(analysis_dir, results_dir, reference_ipynb)
+    create_and_run_notebooks(analysis_dir, result_dir, reference_ipynb)
